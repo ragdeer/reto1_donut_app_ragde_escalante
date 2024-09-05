8f26b2c6da576db68c90799a3a7167bb54b29d83
ragdeer <ragdeescalante65@gmail.com>
2024-09-05 15:19:10 -0600

    Añadiendo texto principal

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,13 +8,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        leading: Icon(Icons.menu, color: Colors.grey[800])
-      ),
+        leading: Padding(
+          padding: const EdgeInsets.only(left:24.0),
+          child: IconButton(icon: Icon(Icons.menu, color: Colors.grey[800], size: 36), onPressed: () { print('Boton oh si cedro'); },
+          ),
+        ),
+        actions: [Padding(
+          padding: const EdgeInsets.only(right:24.0),
+          child: IconButton(icon: Icon(Icons.person, color:Colors.grey[800],size: 36), onPressed: () { print('Oh si ulises'); },),
+        )],
+        ),
+        body: const Column(children: [
+          Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Row(
+              children: [
+                Text('I want to ', style: TextStyle(fontSize: 32),),
+                Text('Eat', style: TextStyle(fontSize: 32, fontWeight:FontWeight.bold, decoration: TextDecoration.underline), 
+                ),
+              ],
+            ),
+          )
+          //TAB BAR
+          //TAB BAR VIEW 
+        ],),
     );
   }
 }
